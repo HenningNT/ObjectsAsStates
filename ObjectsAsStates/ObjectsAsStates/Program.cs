c764102782a07de63915bae23fc506046a25c49c
Henning Torsteinsen <henning.torsteinsen@visuray.com>
2019-08-05 22:09:17 +0200

    Created project

---
+++ ObjectsAsStates/ObjectsAsStates/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ObjectsAsStates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
