f7e9cb4fe1ccf59d094cc4167064ea21a1de17e8
Henning Torsteinsen <henning.torsteinsen@bouvet.no>
2019-08-06 07:00:01 +0200

    Added a bit more to the state classes. Now they have names, and access the trigger.

--- ObjectsAsStates/ObjectsAsStates/Program.cs
+++ ObjectsAsStates/ObjectsAsStates/Program.cs
@@ -25,9 +25,9 @@ namespace ObjectsAsStates
     {
         // All state object, and all trigger objects, need to be instantiated before we
         // can use them when Configuring the state machine.
-        private readonly ObjectAsState _initialState = new ObjectAsState();
-        private readonly ObjectAsState _someState = new ObjectAsState();
-        private readonly AnotherObjectAsState _anotherState = new AnotherObjectAsState();
+        private readonly ObjectAsState _initialState = new ObjectAsState("");
+        private readonly ObjectAsState _someState = new ObjectAsState("SomeState");
+        private readonly AnotherObjectAsState _anotherState = new AnotherObjectAsState("AnotherState");
 
         private readonly ObjectAsTrigger _someTrigger = new ObjectAsTrigger();
         private readonly AnotherObjectAsTrigger _anotherTrigger = new AnotherObjectAsTrigger();
@@ -57,35 +57,60 @@ namespace ObjectsAsStates
                 .Permit(_someTrigger, _someState);
 
             _stateMachine.Configure(_someState)
-                .OnEntry(() => Console.WriteLine("State machine is now in '_someState'"))
+                .OnEntry(_someState.OnEntry)
                 .Permit(_anotherTrigger, _anotherState);
 
             _stateMachine.Configure(_anotherState)
-                .OnEntry(() => Console.WriteLine("State machine is now in '_anotherState'"))
+                .OnEntry((t) =>  _anotherState.OnEntry(t))
                 .Permit(_someTrigger, _someState);
         }
     }
 
     public abstract class StateClass
-    { }
+    {
+        protected readonly string _stateName;
+
+        protected StateClass(string stateName)
+        {
+            _stateName = stateName;
+        }
+    }
 
     public class ObjectAsState : StateClass
     {
+
+        public ObjectAsState(string stateName) : base(stateName) { }
+
         // This is a class that you can use as a state.
         // Use it as any other class.
+        public void OnEntry(StateMachine<StateClass, TriggerClass>.Transition t)
+        {
+            Console.WriteLine($"State machine is now in {_stateName}");
+        }
     }
+
     public class AnotherObjectAsState : StateClass
     {
         // This is a class that you can use as a state, if you so fancy.
+
+        public AnotherObjectAsState(string stateName) : base(stateName) { }
+
+        public void OnEntry(StateMachine<StateClass, TriggerClass>.Transition t)
+        {
+            if (t.Trigger is AnotherObjectAsTrigger trigger)
+            {
+                Console.WriteLine($"State machine is now in {_stateName}, and the trigger payload is {trigger.Payload}");
+            }
+        }
     }
 
+    public abstract class TriggerClass    { }
 
-    public abstract class TriggerClass
-    { }
     public class ObjectAsTrigger :  TriggerClass
     {
         // This is a class that you can use as a state, if you so fancy.
     }
+
     public class AnotherObjectAsTrigger : TriggerClass
     {
         // This is a class that you can use as a state, if you so fancy.
